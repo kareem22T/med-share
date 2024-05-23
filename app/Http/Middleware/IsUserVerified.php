9608abe6a2850e54de8fbc1721fdf229121a719a
kareem22T <kotbekareem74@gmail.com>
2024-05-23 17:24:19 +0300

    edits

---
+++ app/Http/Middleware/IsUserVerified.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsUserVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user;
+        if ($user && $user->is_email_verified)
+            return response()->json([
+                "status" => false,
+                "message" => "فشل العملية",
+                "errors" => ["من فضلك قم بتفعيل حسابك اولا"],
+                "data" => [],
+                "notes" => []
+            ]);
+
+        return $next($request);
+    }
+}
